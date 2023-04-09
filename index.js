653e1e8757c596b7113e3b6068d63eed4e8ae580
Lira Artem <artemlira84@gmail.com>
2023-04-09 23:40:15 +0300

    set up password validation and encryption

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
+import { validationResult } from 'express-validator';
+
 
 import { registerValidation } from "./validations/auth.js";
+import UserModel from "./models/User.js";
 
-mongoose.connect('mongodb+srv://artem:artem8887@cluster0.tpn5ogm.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.jcpn6xu.mongodb.net/portfolio?retryWrites=true&w=majority')
   .then(() => console.log('MongoDB OK'))
   .catch((err) => console.log('MongoDB error', err));
 
@@ -12,32 +16,31 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('111 Hello Artem!');
+app.post('/auth/register', registerValidation, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
+  });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
-
-app.post('/auth/register', registerValidation, (req, res) => {
-
-});
-
-// app.post('/auth/login', (req, res) => {
-
-//   const token = jwt.sign({
-//     email: req.body.email,
-//     fullName: 'Artem Lira',
-//   },
-//     'secret123',
-//   );
-//   res.json({
-//     success: true,
-//     token,
-//   })
-// });
 
 app.listen(4444, (err) => {
   if (err) {
     return console.log(err);
   }
-
   console.log('Server OK');
 });
