139507a37f81caa2f7a8728ac6a5af0102365004
Lira Artem <artemlira84@gmail.com>
2023-04-11 02:32:27 +0300

    set up image loading, refactored the code, fixed the CORS bug

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import express from "express";
+import multer from "multer";
+import cors from "cors";
 import mongoose from "mongoose";
 import {
   registerValidation, loginValidation, skillCreateValidation, mediaCreateValidation, contactCreateValidation,
   factCreateValidation, projectCreateValidation
 } from "./validations.js";
-import checkAuth from "./utils/checkAuth.js";
-import * as UserController from './controllers/UserController.js';
-import * as SkillController from './controllers/SkillController.js';
-import * as MediaController from './controllers/MediaController.js';
-import * as ContactController from './controllers/ContactController.js';
-import * as FactController from './controllers/FactController.js';
-import * as ProjectController from './controllers/ProjectController.js';
+import {
+  UserController, SkillController, MediaController, ContactController, FactController, ProjectController
+} from './controllers/index.js';
+import { handleValidationErrors, checkAuth } from './utils/index.js';
 
 
 mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.jcpn6xu.mongodb.net/portfolio?retryWrites=true&w=majority')
@@ -19,36 +18,55 @@ mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.jcpn6xu.mongodb.net/portfo
 
 const app = express();
 
-app.use(express.json());
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-app.post('/auth/login', loginValidation, UserController.login);
-app.post('/auth/register', registerValidation, UserController.register);
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
 app.get('/skills', SkillController.getAll);
-app.post('/skills', checkAuth, skillCreateValidation, SkillController.create);
+app.post('/skills', checkAuth, skillCreateValidation, handleValidationErrors, SkillController.create);
 app.delete('/skills/:id', checkAuth, SkillController.remove);
-app.patch('/skills/:id', checkAuth, SkillController.update);
+app.patch('/skills/:id', checkAuth, skillCreateValidation, handleValidationErrors, SkillController.update);
 
 app.get('/medias', MediaController.getAll);
-app.post('/medias', checkAuth, mediaCreateValidation, MediaController.create);
+app.post('/medias', checkAuth, mediaCreateValidation, handleValidationErrors, MediaController.create);
 app.delete('/medias/:id', checkAuth, MediaController.remove);
-app.patch('/medias/:id', checkAuth, MediaController.update);
+app.patch('/medias/:id', checkAuth, mediaCreateValidation, handleValidationErrors, MediaController.update);
 
 app.get('/contacts', ContactController.getAll);
-app.post('/contacts', checkAuth, contactCreateValidation, ContactController.create);
+app.post('/contacts', checkAuth, contactCreateValidation, handleValidationErrors, ContactController.create);
 app.delete('/contacts/:id', checkAuth, ContactController.remove);
-app.patch('/contacts/:id', checkAuth, ContactController.update);
+app.patch('/contacts/:id', checkAuth, contactCreateValidation, handleValidationErrors, ContactController.update);
 
 app.get('/facts', FactController.getAll);
-app.post('/facts', checkAuth, factCreateValidation, FactController.create);
+app.post('/facts', checkAuth, factCreateValidation, handleValidationErrors, FactController.create);
 app.delete('/facts/:id', checkAuth, FactController.remove);
-app.patch('/facts/:id', checkAuth, FactController.update);
+app.patch('/facts/:id', checkAuth, factCreateValidation, handleValidationErrors, FactController.update);
 
 app.get('/projects', ProjectController.getAll);
-app.post('/projects', checkAuth, projectCreateValidation, ProjectController.create);
+app.post('/projects', checkAuth, projectCreateValidation, handleValidationErrors, ProjectController.create);
 app.delete('/projects/:id', checkAuth, ProjectController.remove);
-app.patch('/projects/:id', checkAuth, ProjectController.update);
+app.patch('/projects/:id', checkAuth, projectCreateValidation, handleValidationErrors, ProjectController.update);
 
 
 app.listen(4444, (err) => {
